26117410749f09de3834e281c485d8ba26c56ec5
bizbaeja <bjgbjg112@gmail.com>
2023-01-02 20:32:00 +0900

    routers

--- src/server.js
+++ src/server.js
@@ -1,33 +1,34 @@
 import express from "express";
+import morgan from "morgan";
+
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  next();
-};
-
-const handleHome = (req, res, next) => {
-  return res.send("I still love you");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welecome to the Private lounge");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const useRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+useRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", useRouter);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
