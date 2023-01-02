cf64ccc6f68f55b89d7909e7c742b047d4cdf524
bizbaeja <bjgbjg112@gmail.com>
2023-01-02 14:22:54 +0900

    req.url

--- src/server.js
+++ src/server.js
@@ -3,14 +3,19 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res, next) => {
   return res.send("I still love you");
 };
 
 const handleLogin = (req, res) => {
   return res.send("Login here");
 };
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
