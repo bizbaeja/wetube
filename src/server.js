66a0f89d055a5ee8a59b6a31cce87c0c0e2025bc
bizbaeja <bjgbjg112@gmail.com>
2023-01-02 14:44:32 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -3,8 +3,16 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
   next();
 };
 
@@ -12,11 +20,14 @@ const handleHome = (req, res, next) => {
   return res.send("I still love you");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here");
+const handleProtected = (req, res) => {
+  return res.send("Welecome to the Private lounge");
 };
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
