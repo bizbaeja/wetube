906096511f12150c23bee69060c9a7958dc228bb
bizbaeja <bjgbjg112@gmail.com>
2023-02-01 09:58:18 +0900

    mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,66 @@
-const fakeUser = {
-  username: "bizbaeja",
-  loggedIn: false,
+// videos 의 이름과 home pug의 in 다음에 오는 인자의 videos 가 같아야 한다.
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("Watch", {
+    pageTitle: `Watching ${video.title}`,
+    video: video,
+  });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editingg: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
 
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
-
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.render("Search");
-export const upload = (req, res) => res.render("upload");
-export const removeVideo = (req, res) => {
-  return res.send("removeVideo");
+export const getUproad = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
