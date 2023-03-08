851c516de4c3248f15ec995f50ed0a936250c623
bizbaeja <bjgbjg112@gmail.com>
2023-03-08 23:12:49 +0900

    heroku fuck

--- src/server.js
+++ src/server.js
@@ -8,6 +8,40 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import apiRouter from "./routers/apiRouter";
 import { localsMiddleWare } from "./middlewares";
+import http from "http";
+import { async } from "regenerator-runtime";
+var port = process.env.PORT || 8000;
+const CORS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Method": "GET,POST,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers":
+    "Content-Types,Access-Control-Allow-Headers, x-test",
+};
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/result4") {
+    res.writeHead(200, {
+      "Content-Type": "applycation/json",
+      ...CORS,
+    });
+    let data = "";
+    await req
+      .on("data", function (chunk) {
+        data += chunk;
+      })
+      .on("end", () => {});
+    res.write(
+      JSON.stringify({
+        message: "itmo308555",
+        "x-result": "baovn",
+        "x-body": "abc",
+      })
+    );
+  }
+  res.end();
+});
+server.listen(port, () => {
+  console.log("Server is running");
+});
 
 const app = express();
 
